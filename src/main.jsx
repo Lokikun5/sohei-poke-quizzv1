d09dc77533dea030e11e44335ea965a60e426f9c
Antonio <quadjovie.antonio@gmail.com>
2025-02-18 00:47:28 +0100

    Fix GitHub Pages routing issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+//import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import Presenter from "./pages/Presenter";
 import "./index.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   { path: "/", element: <App /> },
   { path: "/presentateur", element: <Presenter /> },
 ]);
